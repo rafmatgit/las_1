f4b7344a2328015ad7be40456907638ba88fc9a8
DonRafal <rafalmatusik1@gmail.com>
2021-03-07 19:07:29 +0100

    1 binaries uploaded

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 import "./style.css";
+import { Howl, Howler } from "howler";
+
+const { Howl, Howler } = require("howler");
 
 const images = {
   treasure: "💰",
-  leafs: "🍃"
+  leafs: "🍃",
+  tree: "🌲"
 };
 
 const trees = document.querySelectorAll(".tree");
@@ -16,3 +20,10 @@ trees.forEach(tree => {
     } else tree.innerHTML = images.leafs;
   });
 });
+
+const sound = new Howl({
+  src: [
+    "C:Users\rafalOneDrivePulpitProgramowanieCoolio_feat_LV_-_Gangstas_Paradise_Qoret.com.mp3"
+  ]
+});
+sound.play();
