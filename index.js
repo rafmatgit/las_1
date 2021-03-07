9d5ab33be2b812beba753c5c35e5c2363241edfc
DonRafal <rafalmatusik1@gmail.com>
2021-03-07 19:22:37 +0100

    2 files modified, 1 added, 1 deleted

--- index.js
+++ index.js
@@ -3,6 +3,13 @@ import { Howl, Howler } from "howler";
 
 const { Howl, Howler } = require("howler");
 
+const sound = new Howl({
+  src: ["./sound.mp3"],
+  onload() {
+    console.log(1);
+  }
+});
+
 const images = {
   treasure: "💰",
   leafs: "🍃",
@@ -15,15 +22,9 @@ trees.forEach(tree => {
   tree.addEventListener("mouseleave", e => (tree.innerHTML = images.tree));
   tree.addEventListener("mouseover", e => {
     if (tree.classList.contains("treasure")) {
+      sound.play();
       tree.innerHTML = images.treasure;
       alert("You Win! Congratz!");
     } else tree.innerHTML = images.leafs;
   });
 });
-
-const sound = new Howl({
-  src: [
-    "C:Users\rafalOneDrivePulpitProgramowanieCoolio_feat_LV_-_Gangstas_Paradise_Qoret.com.mp3"
-  ]
-});
-sound.play();
