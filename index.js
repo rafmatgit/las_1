ad65f0829d7ec5c5870ae4b5e669a54bc8361213
DonRafal <rafalmatusik1@gmail.com>
2021-03-07 18:49:49 +0100

    1 file modified

--- index.js
+++ index.js
@@ -2,10 +2,19 @@ import "./style.css";
 
 const images = {
   treasure: "💰",
-  leafs: "🍃"
+  leafs: "🍃",
+  tree: "🌲"
 };
 
 const trees = document.querySelectorAll(".tree");
 const treasure = document.querySelector(".treasure");
 
-
+trees.forEach(tree => {
+  tree.addEventListener("mouseleave", e => (tree.innerHTML = images.tree));
+  tree.addEventListener("mouseover", e => {
+    if (tree.classList.contains("treasure")) {
+      tree.innerHTML = images.treasure;
+      alert("You Win! Congratz!");
+    } else tree.innerHTML = images.leafs;
+  });
+});
